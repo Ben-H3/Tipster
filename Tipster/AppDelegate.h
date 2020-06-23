175d9cfb74b58a9f1b7417e82a841ec45608a921
Benjamin Charles Hora <bhora@fb.com>
2020-06-23 13:11:32 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Benjamin Charles Hora on 6/23/20.
+//  Copyright © 2020 Ben Hora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
